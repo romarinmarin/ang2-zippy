4aa3d178b85193f8936de3ea2b5ac8f8b6e15eaa
Admin <admin@MacMini.local>
2016-11-10 15:46:21 +0100

    add toggle function

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-zippy',
+  selector: 'my-zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css']
 })
 export class ZippyComponent implements OnInit {
 
-  constructor() { }
+  isVisible : boolean = true;
+
+  constructor() {
+  }
+
+  toggle() {
+    this.isVisible = !this.isVisible;
+  }
 
   ngOnInit() {
   }
 
 }
+
